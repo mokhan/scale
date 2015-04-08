c7acbcacfc49dcc7b757b58c9eca7cb353c83cdf
mo khan <mo@mokhan.ca>
2015-04-08 11:27:40 -0600

    extract attributes for svg.

--- lib/scale/svg.rb
+++ lib/scale/svg.rb
@@ -4,20 +4,29 @@ module Scale
   class SVG
     attr_accessor :width, :height
 
+    def initialize(width: nil, height: nil)
+      @width, @height = width, height
+    end
+
     def to_xml
       builder = Nokogiri::XML::Builder.new do |xml|
-        attributes = {
-          version: "1.1", 
-          baseProfile: "full", 
-          xmlns: "http://www.w3.org/2000/svg",
-        }
-        attributes[:width] = width unless width.nil?
-        attributes[:height] = height unless height.nil?
-
         xml.svg(attributes) do
         end
       end
       builder.to_xml
+    end
+
+    private
+
+    def attributes
+      attributes = {
+        version: "1.1", 
+        baseProfile: "full", 
+        xmlns: "http://www.w3.org/2000/svg",
+      }
+      attributes[:width] = width unless width.nil?
+      attributes[:height] = height unless height.nil?
+      attributes
     end
   end
 end
